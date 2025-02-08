342331dff56aa3a12495e5217f35c6e05b282485
jansencodez <nyamwecliff@gmail.com>
2025-02-08 10:21:02 +0300

    fix: images not displayed

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true, //  if images are not displaying in production
-  },
+  /* config options here */
 };
 
 export default nextConfig;
